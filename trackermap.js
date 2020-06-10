afc84986d76bbcd0bc77b951be6cf050f6168de6
Thibault <kervarecht@gmail.com>
2020-06-09 23:08:56 -0400

    CSV parser works pretty poorly, need to update it to be smarter. Doc also takes forever to load since it gets entirely loaded into memory.  The endpoint takes filters so will be worth adding those to the map

--- trackermap.js
+++ trackermap.js
@@ -5,7 +5,8 @@ var getFreedomTrackerInfo = function () {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function () {
 		if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("demo").innerHTML = this.responseText;
+			
+			document.getElementById("demo").innerHTML = parseCSV(this.response);
 		}
 	};
 
@@ -14,6 +15,26 @@ var getFreedomTrackerInfo = function () {
 };
 
 //Function to parse the CSV once it's done
+//gently updated from https://stackoverflow.com/questions/27979002/convert-csv-data-into-json-format-using-javascript
 var parseCSV = function (csv) {
+	var lines = csv.split("\n");
 
+	var result = [];
+	var headers = lines[0].split(",");
+
+	for (var i = 1; i < lines.length; i++) {
+
+		var obj = {};
+		var currentline = lines[i].split(","); //hoping the commas are escaped otherwise this won't work
+
+		for (var j = 0; j < headers.length; j++) {
+			obj[headers[j]] = currentline[j];
+		}
+
+		result.push(obj);
+
+	}
+	return JSON.stringify(result);
 }
+
+getFreedomTrackerInfo();
